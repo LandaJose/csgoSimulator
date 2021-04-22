b8cf30d0085682342bc821cc24a301c83ef46504
Kgiang6 <70337556+Kgiang6@users.noreply.github.com>
2021-04-22 01:59:36 -0700

    "framework of stack navigation"

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {HomeScreenStack} from './Navigator/StackNavigation'
 
-export default function App() {
+const App= props => {
   return (
-    <View style={styles.container}>
-      <Text>This is a test.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <HomeScreenStack/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
